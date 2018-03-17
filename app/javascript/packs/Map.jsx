94b368cb93d37db4b5857bc9a264c32dad9fc9df
maayan.bachar <maayan.bachar@wework.com>
2018-03-17 18:26:04 +0200

    some css changes

--- app/javascript/packs/Map.jsx
+++ app/javascript/packs/Map.jsx
@@ -22,7 +22,7 @@ export function Map(){
                 isMarkerShown={false}
                 googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyBUGpNjQ-H4t45okAePNtVfQ9odlSfgiHM&v=3.exp&libraries=geometry,drawing,places"
                 loadingElement={<div style={{ height: `100%` }} />}
-                containerElement={<div style={{ height: `100%`, width: '500px' }} />}
+                containerElement={<div style={{ height: `100%`, width: '100%' }} />}
                 mapElement={<div style={{ height: `100%` }} />}
             />
         </div>
